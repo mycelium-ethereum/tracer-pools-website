c06014f47aea980f8e5dc2a58a4acb36ea2fb1ab
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-06 22:52:44 +1000

    Small fixes for 640px breakpoint

--- src/components/wallet-animation.js
+++ src/components/wallet-animation.js
@@ -39,7 +39,7 @@ const WalletAnimation = () => {
             Browse Tokens
           </Button>
         </div>
-        <div className="w-full absolute 2xl:h-60 xl:-bottom-32 lg:h-48 lg:-bottom-12 sm:-bottom-36 h-48 -bottom-60">
+        <div className="w-full absolute 2xl:h-60 xl:-bottom-32 lg:h-48 lg:-bottom-12 sm:-bottom-60 h-48 -bottom-60">
           <TokenSlider className="slider-left w-2/4 overflow-hidden bottom-0 h-full absolute lg:-left-16 -left-16 z-10" />
           <div className="absolute 2xl:bottom-8 2xl:w-96 lg:w-72 lg:bottom-6 left-2/4 transform -translate-x-1/2 h-full sm:w-60 w-48 z-20">
             <picture className="flex flex-col justify-center items-center relative w-full">
