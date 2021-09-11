61fcce0214422dda8c2dc6159d0efb4d91dbe5f9
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-11 15:33:10 +1000

    Resize policy text to match design

--- src/components/policy-text.js
+++ src/components/policy-text.js
@@ -120,13 +120,13 @@ const PrivacyText = () => {
             <h1 className="text-black text-4xl mb-4 font-semibold">
               Privacy Policy
             </h1>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               In this Privacy Policy, ‘us’ ‘we’ or ‘our’ means Tracer DAO. We
               are committed to respecting your privacy. Our Privacy Policy sets
               out how we collect, use, store and disclose your personal
               information.
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               This Privacy Policy applies to our services, which include the
               services we provide on https://tracer.finance/ or any other
               websites, pages, features, or content we own or operate or when
@@ -134,7 +134,7 @@ const PrivacyText = () => {
               this Privacy Policy, do not access or use the services, websites,
               or any other aspect of our business.
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               By providing personal information to us, you consent to our
               collection, use and disclosure of your personal information in
               accordance with this Privacy Policy and any other arrangements
@@ -143,19 +143,19 @@ const PrivacyText = () => {
               you to check our website periodically to ensure that you are aware
               of our current Privacy Policy.
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               Personal information includes information or an opinion about an
               individual that is reasonably identifiable.
             </p>
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>How do we collect personal information?</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may collect these types of personal information either directly
               from you, or from third parties. We may collect this information
               when you:
             </p>
-            <ul className="list-disc list-outside pl-4 mb-4">
+            <ul className="text-sm list-disc list-outside pl-4 mb-4">
               <li>register on our website or app;</li>
               <li>
                 communicate with us through correspondence, chats, email, or
@@ -169,11 +169,11 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Why do we collect, use and disclose personal information?</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may collect, hold, use and disclose your personal information
               for the following purposes:
             </p>
-            <ul className="list-disc list-outside pl-4 mb-4">
+            <ul className="text-sm list-disc list-outside pl-4 mb-4">
               <li>
                 to enable you to access and use our website, services and app;
               </li>
@@ -206,7 +206,7 @@ const PrivacyText = () => {
                 with third parties; and to consider your employment application.
               </li>
             </ul>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may also disclose your personal information to a trusted third
               party who also holds other information about you. This third party
               may combine that information in order to enable it and us to
@@ -217,7 +217,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Using our website and cookies</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may collect personal information about you when you use and
               access our website.
               <br /> While we do not use browsing information to identify you
@@ -229,7 +229,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Integrated third party services</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               Various third party services are integrated with our website,
               including MetaMask. Unless expressly stated otherwise, we are not
               responsible for the privacy practices of integrated third party
@@ -243,7 +243,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Accessing or correcting your personal information</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               You can access the personal information we hold about you by
               contacting us at hello@tracer.finance. Sometimes, we may not be
               able to provide you with access to all of your personal
@@ -256,7 +256,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Contact Us</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               For further information about our Privacy Policy or practices, or
               to access or correct your personal information, or make a
               complaint, please contact us at
@@ -265,16 +265,16 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>What personal information do we collect?</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               For further information about our Privacy Policy or practices, or
               to access or correct your personal information, or make a
               complaint, please contact us at
               <a href="mailto:hello@tracer.finance">hello@tracer.finance</a>.
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may collect the following types of personal information:
             </p>
-            <ul className="list-disc list-outside pl-4 mb-4">
+            <ul className="text-sm list-disc list-outside pl-4 mb-4">
               <li>contact information, such as your email address;</li>
               <li>
                 transactional information, such as information about the
@@ -320,7 +320,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Do we use your personal information for direct marketing?</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We and/or our carefully selected business partners may send you
               direct marketing communications and information about our service
               and products. This may take the form of emails, Discord messages,
@@ -333,11 +333,11 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>To whom do we disclose your personal information?</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may disclose personal information for the purposes described in
               this privacy policy to:
             </p>
-            <ul className="list-disc list-outside pl-4 mb-4">
+            <ul className="text-sm list-disc list-outside pl-4 mb-4">
               <li>
                 third party suppliers and service providers (including providers
                 for the operation of our websites and/or our business or in
@@ -367,7 +367,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Disclosure of personal information</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               When you provide your personal information to us, you consent to
               the disclosure of your information globally. We will, however,
               take reasonable steps to ensure that any overseas recipient will
@@ -378,7 +378,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Security</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               We may hold your personal information in either electronic or hard
               copy form. We take reasonable steps to protect your personal
               information from misuse, interference and loss, as well as
@@ -390,7 +390,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Links</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               Our website may contain links to websites operated by third
               parties. Those links are provided for convenience and may not
               remain current or be maintained. Unless expressly stated
@@ -404,7 +404,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Making a complaint</b>
             </p>
-            <p className="mb-4">
+            <p className="text-sm mb-4">
               If you think we have breached the OECD Guidelines on the
               Protection of Privacy and Transborder Flows of Personal Data, or
               you wish to make a complaint about the way we have handled your
@@ -419,7 +419,7 @@ const PrivacyText = () => {
             <p className="mb-4 mt-8 font-semibold text-black text-lg">
               <b>Effective</b>
             </p>
-            <p className="mb-4">23 April, 2021</p>
+            <p className="text-sm mb-4">23 April, 2021</p>
           </div>
         </div>
       </section>
