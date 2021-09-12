d08f47d2c1c369c73aba2c2a6ab65d1df111ca8d
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-12 11:50:11 +1000

    Corrected spacing on Learn page for tablet/mobile

--- src/components/learn-header.js
+++ src/components/learn-header.js
@@ -4,7 +4,7 @@ import React from "react";
 const LearnHeader = () => {
   return (
     <>
-      <header className="h-96 flex items-end z-20 relative">
+      <header className="h-96 flex md:items-end md:pt-0 pt-20 items-center z-20 relative">
         <div className="container w-full mx-auto flex flex-col justify-center text-center pt-24 lg:px-0 px-4">
           <small className="md:text-3xl text-xl color-blue font-bold">Learn</small>
           <h1 className="font-black md:text-6xl text-4xl text-white mb-2">Tracer Research</h1>
--- src/components/research-papers.js
+++ src/components/research-papers.js
@@ -33,7 +33,7 @@ const ResearchPapers = () => {
     <>
       <section className="h-full w-full z-20 relative mt-16">
         {/* <div class="absolute top-0 left-0 w-full h-full z-0 overflow-hidden bg-blue opacity-30"></div> */}
-        <div className="container relative w-full mx-auto pb-12 xl:px-0 px-4 grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-12 pt-3">
+        <div className="container relative w-full mx-auto pb-12 xl:px-0 px-4 grid md:grid-cols-3 grid-cols-1 md:gap-8 lg:pt-5 md:pt-13 pt-5 gap-12 ">
           <PaperDownload {...poolsProps}/>
           <PaperDownload {...perpetualSwapsProps}/>
           <PaperDownload {...p2pFinanceProps}/>
