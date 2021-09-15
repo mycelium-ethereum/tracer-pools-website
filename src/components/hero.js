64ef4a45153982236385e450b71ad138159662c9
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-15 20:52:02 +1000

    Adjust laptop and arrow position

--- src/components/hero.js
+++ src/components/hero.js
@@ -36,28 +36,28 @@ const Hero = () => {
               </div>
             </a>
           </header>
-          <div className="2xl:max-w-screen-xl xl:max-w-screen-lg 2xl:-bottom-96 xl:-bottom-80 lg:-bottom-72 md:-bottom-32 sm:-bottom-32 sm:px-24 absolute px-6 w-full -bottom-10 z-20">
+          <div className="2xl:max-w-screen-xl xl:max-w-screen-lg 2xl:-bottom-96 xl:-bottom-80 lg:-bottom-72 md:-bottom-32 sm:-bottom-32 sm:px-24 absolute px-6 w-full -bottom-20 z-20">
             <img
               className="w-full"
               src={TracerLaptop}
               alt="Tracer Laptop View"
             />
           </div>
-        </div>
-        <AnchorLink
-          to="/#steps"
-          className="absolute 2xl:right-48 xl:right-30 lg:right-32 sm:bottom-32 sm:right-16 border-2 border-white rounded-full bottom-6 right-6 w-14 h-14 bg-white bg-opacity-20 transition-colors duration-500 text-white hover:bg-white fill-current stroke-current hover:text-blue-400 z-20"
-        >
-          <svg
-            width="16"
-            height="10"
-            viewBox="0 0 16 10"
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          <AnchorLink
+            to="/#steps"
+            className="absolute sm:right-0 right-8 border-2 border-white rounded-full lg:bottom-12 bottom-6 w-14 h-14 bg-white bg-opacity-20 transition-colors duration-500 text-white hover:bg-white fill-current stroke-current hover:text-blue-400 z-20"
           >
-            <path d="M1.84933 0.235229L7.69959 6.0855L13.5499 0.235229L15.3406 2.02597L7.6988 9.66775L0.0585938 2.02597L1.84933 0.235229Z" />
-          </svg>
-        </AnchorLink>
+            <svg
+              width="16"
+              height="10"
+              viewBox="0 0 16 10"
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            >
+              <path d="M1.84933 0.235229L7.69959 6.0855L13.5499 0.235229L15.3406 2.02597L7.6988 9.66775L0.0585938 2.02597L1.84933 0.235229Z" />
+            </svg>
+          </AnchorLink>
+        </div>
       </section>
       <div className="2xl:h-96 lg:h-80 h-40 bg-blue" />
     </>
