129a480f091f0693720dfd50cec29745cb4f226b
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-15 21:01:01 +1000

    Fix parallax overflow

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -35,7 +35,7 @@ const BackgroundParallax = () => {
       <div
         className={
           (fixed ? "fixed" : "absolute") +
-          " left-0 w-screen origin-top xl:transform-none xl:left-0 lg:transform-gpu lg:scale-75 lg:left-1/2 lg:-translate-x-1/2 h-full z-10 pointer-events-none sm:top-0 -top-32"
+          " left-0 w-screen origin-top xl:transform-none xl:left-0 lg:transform-gpu lg:scale-75 lg:left-1/2 lg:-translate-x-1/2 h-screen z-10 pointer-events-none sm:top-0 -top-32"
         }
       >
         <div className="absolute tile-bg">
