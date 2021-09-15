a8f46ad0363f1864d00bd9a63abf11980199b183
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-15 22:19:38 +1000

    Refine 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,29 @@
 import React from "react";
-
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => {
-    return (
-      <>
-        <Layout>
-          <SEO title="404: Not found" />
-          <section className="h-screen w-full relative z-10">
-            <div className="container w-full h-screen min-h-body relative flex items-center justify-center xl:px-0 sm:pt-18 pt-16 px-4 mx-auto">
-              <header className="text-white text-6xl font-black">
-                <h1>This page does not exist.</h1>
-              </header>
-            </div>
-          </section>
-        </Layout>
-      </>
-    );
-}
+  return (
+    <>
+      <Layout>
+        <SEO title="Coming Soon" />
+        <section className="h-screen w-full relative z-10">
+          <div className="container w-full h-screen min-h-body relative flex items-center justify-center xl:px-0 sm:pt-18 pt-16 px-4 mx-auto">
+            <header className="text-white text-center">
+              <h1 className="sm:text-7xl text-5xl font-black">This page does not exist</h1>
+              <Link
+                to="/"
+                className="tracer-btn text-normal font-normal rounded-lg h-11 w-48 text-center transition-all duration-500 border-2 border-white mx-auto mt-12"
+              >
+                Back to home
+              </Link>
+            </header>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
 
 export default NotFoundPage;
