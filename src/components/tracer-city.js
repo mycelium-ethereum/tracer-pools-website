b82327dbe5cecc2360dff2c307ab0265386691b7
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-20 11:32:37 +1000

    Added conditional rendering of video on desktop

--- src/components/tracer-city.js
+++ src/components/tracer-city.js
@@ -36,25 +36,32 @@ const TracerCity = () => {
               </small>
             </div>
           </div>
-          <video
-            muted
-            loop="loop"
-            autoPlay
-            playsInline
-            disableRemotePlayback={true}
-            disablePictureInPicture
-            className="h-fit lg:opacity-0 transition-opacity duration-700 h-full col-span-5 transform scale-110 relative rounded-xl overflow-hidden"
-            // poster={TracerCityPNG}
-            poster={showThumbnail ? TracerCityPNG : undefined}
-            onLoadedData={() => setShowThumbnail(false)}
-          >
-            {/* <source src={TracerCityVideoMP4} type="video/mp4; codecs='hvc1'" /> */}
-            {browserName !== "Safari" && (
+          {browserName.toString() !== "Safari" ? (
+            <video
+              muted
+              loop="loop"
+              autoPlay
+              playsInline
+              disableRemotePlayback={true}
+              disablePictureInPicture
+              className="h-fit lg:opacity-0 transition-opacity duration-700 h-full col-span-5 transform scale-110 relative rounded-xl overflow-hidden"
+              id="tracer-city"
+            >
               <source src={TracerCityVideoWEBM} type="video/webm" />
-            )}
-            {console.log(browserName)}
-            {console.log(browserName !== "Safari")}
-          </video>
+            </video>
+          ) : (
+            <video
+              muted
+              loop="loop"
+              autoPlay
+              playsInline
+              disableRemotePlayback={true}
+              disablePictureInPicture
+              className="h-fit lg:opacity-0 transition-opacity duration-700 h-full col-span-5 transform scale-110 relative rounded-xl overflow-hidden"
+              poster={TracerCityPNG}
+              id="tracer-city"
+            ></video>
+          )}
         </div>
         <img
           className="absolute h-full -right-48 -bottom-12 z-0 w-full md:block hidden"
@@ -72,22 +79,32 @@ const TracerCity = () => {
         className="panel w-full lg:h-auto h-auto staking-section flex lg:items-center relative items-start xl:pb-12 lg:pt-0 pt-24 bg-blue z-10 overflow-hidden"
       >
         <div className="container flex justify-between mx-auto w-full h-full xl:px-0 px-4 lg:flex-row flex-col-reverse relative z-10">
-          <video
-            muted
-            loop="loop"
-            autoPlay
-            playsInline
-            disableRemotePlayback={true}
-            disablePictureInPicture
-            id="tracer-building"
-            className="h-fit lg:opacity-0 transition-opacity duration-700 lg:w-1/2 lg:mx-0 mx-auto w-3/4 md:mt-0 mt-6 lg:static lg:left-0 relative"
-            poster={showBuildingThumbnail ? TracerBuildingPNG : undefined}
-            onLoadedData={() => setShowBuildingThumbnail(false)}
-          >
-            {browserName !== "Safari" && (
+          {browserName.toString() !== "Safari" ? (
+            <video
+              muted
+              loop="loop"
+              autoPlay
+              playsInline
+              disableRemotePlayback={true}
+              disablePictureInPicture
+              id="tracer-building"
+              className="h-fit lg:opacity-0 transition-opacity duration-700 lg:w-1/2 lg:mx-0 mx-auto w-3/4 md:mt-0 mt-6 lg:static lg:left-0 relative"
+            >
               <source src={TracerBuildingWEBM} type="video/webm" />
-            )}
-          </video>
+            </video>
+          ) : (
+            <video
+              muted
+              loop="loop"
+              autoPlay
+              playsInline
+              disableRemotePlayback={true}
+              disablePictureInPicture
+              id="tracer-building"
+              className="h-fit lg:opacity-0 transition-opacity duration-700 lg:w-1/2 lg:mx-0 mx-auto w-3/4 md:mt-0 mt-6 lg:static lg:left-0 relative"
+              poster={TracerBuildingPNG}
+            ></video>
+          )}
           <div
             className="2xl:mt-52 xl:mt-80 lg:pl-12 lg:mt-48 lg:pb-0 lg:pr-12 lg:absolute lg:text-left left-1/2 mx-auto px-4 w-full force-flex flex-col justify-center lg:items-start items-center z-10 h-auto text-center"
             data-stellar-ratio="1.4"
