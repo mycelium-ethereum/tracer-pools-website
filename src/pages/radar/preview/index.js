8aac80854bb9f5049f3118cac6e870038ce2245d
Tim Jones <t27.jones@connect.qut.edu.au>
2021-09-27 10:05:13 +1000

    Added preview component

--- src/pages/radar/preview/index.js
+++ src/pages/radar/preview/index.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Components
 import Layout from "../../../components/layout";
 import SEO from "../../../components/seo";
 import BlogContent from "../../../components/blog-template-content";
 
-// async function loadCorgis() {
-//   const data = await fetch("/.netlify/functions/load-corgis")
-//     .then((res) => res.json())
-//     .catch((err) => console.error(err));
-//   console.log(data);
-// }
-const Preview = () => {
+const Preview = (props) => {
+  const [postPreview, setPostPreview] = useState({
+    title: "",
+    image: {
+      formats: {
+        medium: {
+          url: "",
+        },
+      },
+    },
+    description: "",
+  });
+  async function getPost() {
+    // Get blog post ID from URL parameters
+    const params = new URLSearchParams(props.location.search);
+    const id = parseInt(params.get("id"));
+    const data = await fetch(
+      "https://mycelium-content.uc.r.appspot.com/tracer-blogs?_publicationState=preview&published_at_null=true"
+    )
+      .then((res) => res.json())
+      .catch((err) => console.error(err));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        setPostPreview(data[i]);
+        break;
+      }
+    }
+  }
+  useEffect(() => {
+    getPost();
+  }, []);
   return (
     <>
       <SEO
-      //   title={this.props.data.strapiTracerBlogs.title}
-      //   image={this.props.data.strapiTracerBlogs.image[0].formats.medium.url}
-      //   description={this.props.data.strapiTracerBlogs.description}
+        title={postPreview.title}
+        // image={postPreview.image[0].formats.medium.url}
+        // description={postPreview.description}
       />
       <Layout>
-        {/* <BlogContent data={this.props.data.strapiTracerBlogs} /> */}
+        <BlogContent data={postPreview} />
       </Layout>
     </>
   );
