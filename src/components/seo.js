8d146097187062a78ab5e76774837fecb45087d6
Tim Jones <t27.jones@connect.qut.edu.au>
2021-10-08 11:04:17 +1000

    Remove old Plausible component

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
-import Plausible from "./plausible-script";
 
 // Images
 import OpenGraphLogo from "/static/img/general/open-graph.png";
