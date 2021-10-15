830492d52b40ab5b05552f539e82c02cd7d9db6e
Tim Jones <t27.jones@connect.qut.edu.au>
2021-10-15 18:14:10 +1000

    Apply initial pagination to Blog Posts

--- src/components/blog-posts.js
+++ src/components/blog-posts.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StaticQuery, graphql } from "gatsby";
+import ReactPaginate from "react-paginate";
 import PostLink from "./blog-post-link";
 
 const query = graphql`
@@ -25,12 +26,40 @@ const query = graphql`
   }
 `;
 const BlogPosts = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPosts, setCurrentPosts] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const PER_PAGE = 9;
+  const offset = currentPage * PER_PAGE;
+
+  const loadData = (data) => {
+    const curPosts = data
+      .slice(offset, offset + PER_PAGE)
+      .map((node, i) => <PostLink data={node} key={i} />);
+    setCurrentPosts(curPosts);
+    setPageCount(Math.ceil(data.length / PER_PAGE));
+  };
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+  };
+
   const sortByDate = (a, b) => {
     return (
       new Date(b.node.publish_date).getTime() -
       new Date(a.node.publish_date).getTime()
     );
   };
+
+  useEffect(() => {
+    loadData(posts);
+  }, [posts]);
+
+  useEffect(() => {
+    loadData(posts);
+  }, [currentPage]);
+
   return (
     <>
       <section className="h-full w-full z-20 relative bg-white select-dark">
@@ -38,18 +67,44 @@ const BlogPosts = () => {
           <h1 className="font-semibold text-3xl text-center mb-8">
             Latest Articles
           </h1>
-          <StaticQuery
-            query={query}
-            render={(data) => (
-              <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
-                {data.allStrapiTracerBlogs.edges
-                  .sort((a, b) => sortByDate(a, b))
-                  .map((node, i) => (
-                    <PostLink data={node} key={i} />
-                  ))}
-              </div>
-            )}
+          <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
+            <StaticQuery
+              query={query}
+              render={(data) =>
+                setPosts(
+                  data.allStrapiTracerBlogs.edges.sort((a, b) =>
+                    sortByDate(a, b)
+                  )
+                )
+              }
+            />
+            {currentPosts}
+          </div>
+          <hr className="mt-12 color-paginationrulegrey" />
+          <ReactPaginate
+            previousLabel={"← Previous"}
+            nextLabel={"Next →"}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={"relative box-border flex justify-center pl-0 mb-12 w-full"}
+            // pageClassName={"relative flex items-center justify-center color-paginationgrey w-9 h-12 transition-colors inactive-link"}
+            pageLinkClassName={"relative flex items-center justify-center color-paginationgrey w-9 h-12 transition-colors inactive-link"}
+            previousLinkClassName={"absolute left-0 py-2"}
+            nextLinkClassName={"absolute right-0 py-2"}
+            disabledClassName={"disabled"}
+            activeLinkClassName={"active-link relative color-tracerdarkblue"}
           />
+          {/* <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={10}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            // onPageChange={handlePageClick()}
+            containerClassName={"pagination"}
+          /> */}
         </div>
       </section>
     </>
