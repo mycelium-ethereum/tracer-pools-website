12cbff9ec465a1bc47b751f294851a5d3d6ff260
Tim Jones <t27.jones@connect.qut.edu.au>
2021-10-16 16:23:48 +1000

    Adjust padding of search results, fix capitalised words not being searched

--- src/components/blog-search.js
+++ src/components/blog-search.js
@@ -64,38 +64,22 @@ const BlogSearch = ({ setShowSearch, showSearch, posts, postTitles }) => {
         }
       >
         <button
-          className="absolute md:top-6 md:right-6 top-3 right-3"
+          className="absolute md:top-6 top-3 right-6"
           onClick={() => closeSearch()}
         >
           <img className="w-6 h-6" src={CloseIcon} />
         </button>
         <div className="container max-w-blog mx-auto md:pt-32 pt-14 md:pb-16 pb-6 lg:px-0 px-4">
-          <div className="relative flex flex-row-reverse items-center w-full md:h-14 md:mb-11 h-10 mb-8 md:px-0 px-2">
+          <div className="relative flex flex-row-reverse items-center w-full md:h-14 md:mb-11 h-10 mb-8 md:px-8">
             <SearchBar
               postTitles={postTitles}
               searchResults={searchResults}
               setSearchTerm={setSearchTerm}
             />
-            <svg
-              width="19"
-              height="20"
-              viewBox="0 0 19 20"
-              fill="none"
-              className="absolute left-4 w-5 h-5 transition-all duration-300"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16.625 17.125L11.875 12.375M13.4583 8.41667C13.4583 9.14441 13.315 9.86503 13.0365 10.5374C12.758 11.2097 12.3498 11.8206 11.8352 12.3352C11.3206 12.8498 10.7097 13.258 10.0374 13.5365C9.36503 13.815 8.64441 13.9583 7.91667 13.9583C7.18892 13.9583 6.46831 13.815 5.79596 13.5365C5.12362 13.258 4.51271 12.8498 3.99812 12.3352C3.48353 11.8206 3.07533 11.2097 2.79683 10.5374C2.51834 9.86503 2.375 9.14441 2.375 8.41667C2.375 6.94693 2.95885 5.53738 3.99812 4.49812C5.03738 3.45885 6.44693 2.875 7.91667 2.875C9.38641 2.875 10.796 3.45885 11.8352 4.49812C12.8745 5.53738 13.4583 6.94693 13.4583 8.41667Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
           </div>
-          <div className="overflow-hidden md:rounded-none rounded-tl-2xl rounded-tr-2xl">
+          <div className="overflow-y-hidden md:rounded-none rounded-tl-2xl rounded-tr-2xl">
             <div
-              className="search-results overflow-y-scroll overflow-x-hidden transition-opacity duration-500 md:block sm:grid-cols-2 grid grid-cols-1 gap-8 pb-8 px-2"
+              className="search-results overflow-y-scroll overflow-x-hidden transition-opacity duration-500 md:block sm:grid-cols-2 grid grid-cols-1 gap-8 pb-8 md:px-8"
               ref={searchResults}
             >
               {postResults}
--- src/components/blog-searchbar.js
+++ src/components/blog-searchbar.js
@@ -9,7 +9,7 @@ const SearchBar = ({ postTitles, searchResults, setSearchTerm }) => {
     searchResults.current.classList.add("opacity-0");
     onChange(event);
     setTimeout(() => {
-      setSearchTerm(event.target.value);
+      setSearchTerm(event.target.value.toLowerCase());
       searchResults.current.classList.remove("opacity-0");
     }, 500);
   };
@@ -52,7 +52,7 @@ const SearchBar = ({ postTitles, searchResults, setSearchTerm }) => {
   return (
     <>
       <input
-        className="search-box bg-transparent w-full h-full bg-searchgrey rounded-2xl pl-12 z-0 font-normal text-gray-800"
+        className="search-box bg-transparent w-full h-full bg-searchgrey rounded-2xl md:pl-14 pl-11 z-0 font-normal text-gray-800"
         placeholder="Search"
         name="search-bar"
         type="text"
@@ -63,9 +63,25 @@ const SearchBar = ({ postTitles, searchResults, setSearchTerm }) => {
       <input
         type="text"
         name="search-bar"
-        className="absolute top-0 md:left-0 left-2 bg-transparent w-full h-full rounded-2xl pl-12 font-normal pointer-events-none z-10 opacity-40"
+        className="absolute top-0 md:left-8 left-2 bg-transparent w-full h-full rounded-2xl md:pl-14 pl-11 font-normal pointer-events-none z-10 opacity-40"
         value={suggestion}
       />
+      <svg
+        width="19"
+        height="20"
+        viewBox="0 0 19 20"
+        fill="none"
+        className="absolute md:left-14 left-4 w-5 h-5 transition-all duration-300"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.625 17.125L11.875 12.375M13.4583 8.41667C13.4583 9.14441 13.315 9.86503 13.0365 10.5374C12.758 11.2097 12.3498 11.8206 11.8352 12.3352C11.3206 12.8498 10.7097 13.258 10.0374 13.5365C9.36503 13.815 8.64441 13.9583 7.91667 13.9583C7.18892 13.9583 6.46831 13.815 5.79596 13.5365C5.12362 13.258 4.51271 12.8498 3.99812 12.3352C3.48353 11.8206 3.07533 11.2097 2.79683 10.5374C2.51834 9.86503 2.375 9.14441 2.375 8.41667C2.375 6.94693 2.95885 5.53738 3.99812 4.49812C5.03738 3.45885 6.44693 2.875 7.91667 2.875C9.38641 2.875 10.796 3.45885 11.8352 4.49812C12.8745 5.53738 13.4583 6.94693 13.4583 8.41667Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </>
   );
 };
