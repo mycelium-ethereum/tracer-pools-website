2b485dc122aa2ff7ccafa8d5d03bc695073bc8c0
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-11-05 13:59:07 +1000

    Remove subscription form

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,24 +12,11 @@ import Discord from "/static/img/social-icons/discord-footer.svg";
 import PoolsPDF from "../../static/pdf/Tracer Perpetual Pools.pdf";
 
 const Footer = () => {
-  const isBelowBreakpoint = () => {
-    const width = window.innerWidth;
-    return width < 768;
-  };
   const setCopyrightYear = () => {
     document.getElementById("year").innerHTML = new Date().getFullYear();
   };
 
   const [formOpen, setFormOpen] = useState(false);
-  const checkForm = (e) => {
-    const form = document.getElementById("subscription-form");
-    if (isBelowBreakpoint() && !formOpen) {
-      e.preventDefault();
-      setFormOpen(true);
-    } else if (formOpen && !isBelowBreakpoint()) {
-      form.submit();
-    }
-  };
   const hideForm = (e) => {
     const input = document.getElementById("email");
     const button = document.getElementById("submit-btn");
@@ -37,24 +24,6 @@ const Footer = () => {
     if (!formOpen && target !== input && target !== button) {
       setFormOpen(false);
     }
-  };
-  const [submittedText, setSubmittedText] = useState("Subscribe");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    fetch("https://mycelium.activehosted.com/proc.php", {
-      method: "POST",
-      body: data,
-      mode: "no-cors",
-    })
-      .then((res) => {
-        console.log(res);
-        setSubmittedText("Subscribed!");
-        setTimeout(() => {
-          setSubmittedText("Subscribe");
-        }, 5000);
-      })
-      .catch((error) => console.log("Request failed", error));
   };
   useEffect(() => {
     setCopyrightYear();
@@ -69,52 +38,11 @@ const Footer = () => {
     <footer className="w-full relative overflow-hidden lg:py-10 h-1/2 z-10 bg-blue">
       <img
         id="footer-bg"
-        // xl:-top-96 md:-top-56 sm:-top-44 -top-10
         className="absolute 2xl:-bottom-52 xl:-bottom-36 lg:-bottom-6 md:-bottom-13 sm:bottom-2 sm:top-auto bottom-auto -top-14 left-0 w-full h-auto "
         src={CityBottomDark}
         alt=""
       />
       <div className="container mx-auto relative xl:flex flex-col justify-between z-1 xl:px-0 px-8">
-        <div className="flex flex-col xl:justify-center justify-between items-center lg:w-full lg:pt-8 sm:w-max sm:px-0 w-full h-auto mx-auto pt-6 mb-20">
-          <span className="text-white lg:w-auto lg:mb-6 lg:text-left lg:text-xl font-semibold text-center w-full text-lg">
-            Stay updated on the latest Tracer news
-          </span>
-          <form
-            id="subscription-form"
-            className="2xl:pb-0 lg:mb-48 lg:w-min sm:ml-0 sm:mr-0 mx-auto w-full flex lg:mt-0 mt-4 justify-between md:flex-row flex-col sm:px-0 px-6"
-            onSubmit={(e) => handleSubmit(e)}
-          >
-            <input type="hidden" name="u" value="15" />
-            <input type="hidden" name="f" value="15" />
-            <input type="hidden" name="s" />
-            <input type="hidden" name="c" value="0" />
-            <input type="hidden" name="m" value="0" />
-            <input type="hidden" name="act" value="sub" />
-            <input type="hidden" name="v" value="2" />
-            <input
-              type="email"
-              name="email"
-              autoCapitalize="off"
-              autoComplete="off"
-              autoCorrect="off"
-              className={
-                "w-full h-10 border-0 rounded-lg lg:py-0 lg:w-80 lg:mb-0 md:mr-4 mb-6 py-2 pl-4 pr-4 max-w-xs mx-auto " +
-                (formOpen ? "active" : "")
-              }
-              id="email"
-              placeholder="Email"
-              required
-            />
-            <button
-              onClick={(e) => checkForm(e)}
-              id="submit-btn"
-              type="submit"
-              className="tracer-btn.subscribe h-10 rounded-lg font-medium flex items-center justify-center bg-blue-600 text-white lg:w-32 lg:mt-0 lg:mb-0 md:w-32 mb-8 w-full max-w-xs mx-auto"
-            >
-              {submittedText}
-            </button>
-          </form>
-        </div>
         <div className="h-auto w-full flex lg:flex-row flex-col justify-between sm:items-center items-start">
           <span className="text-base font-normal text-white">
             &copy; <span id="year" /> Tracer DAO
