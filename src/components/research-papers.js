c720bc4ddb3e2ac8e016297c6e177522af7398de
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-11-10 10:58:02 +1000

    Add new litepaper

--- src/components/research-papers.js
+++ src/components/research-papers.js
@@ -8,12 +8,20 @@ import P2PFinanceHeader from "../../static/img/learn/p2p/p2p-finance-header.png"
 import PoolsLitepaper from "../../static/img/learn/pools/pools-litepaper.png";
 import PerpetualSwapsPaper from "../../static/img/learn/swaps/perpetual-swaps-paper.png";
 import P2PFinancePaper from "../../static/img/learn/p2p/p2p-finance-paper.png";
+import PurchasePaper from "../../static/img/learn/purchase/purchase-paper.png";
 // PDFs
 import PoolsPDF from "../../static/pdf/Tracer Perpetual Pools.pdf";
 import SwapsPDF from "../../static/pdf/Tracer Perpetual Swaps.pdf";
 import P2PPDF from "../../static/pdf/Tracer Peer-to-Peer Finance.pdf";
+import PurchasePDF from "../../static/pdf/Purchasing Power Stablecoin.pdf";
 
 const ResearchPapers = () => {
+  const purchaseProps = {
+    title: "Purchasing Power Stablecoin",
+    header: PoolsHeader,
+    image: PurchasePaper,
+    link: PurchasePDF,
+  }
   const poolsProps = {
     title: "Perpetual Pools",
     header: PoolsHeader,
@@ -35,7 +43,8 @@ const ResearchPapers = () => {
   return (
     <>
       <section className="h-full w-full z-20 relative mt-16">
-        <div className="container relative w-full mx-auto pb-12 xl:px-0 px-4 grid md:grid-cols-3 grid-cols-1 md:gap-4 lg:pt-5 md:pt-13 pt-5 gap-12 ">
+        <div className="container relative w-full mx-auto pb-12 xl:px-0 px-4 grid md:grid-cols-2 grid-cols-1 md:gap-20 lg:pt-5 md:pt-13 pt-5 gap-12">
+          <PaperDownload {...purchaseProps} />
           <PaperDownload {...poolsProps} />
           <PaperDownload {...swapsProps} />
           <PaperDownload {...p2pProps} />
