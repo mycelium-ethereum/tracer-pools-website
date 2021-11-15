66488f5ca899dc69bd1250870ec53b1b05c70fd1
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-11-15 23:34:41 +1000

    New learn paper tile header

--- src/components/paper-download.js
+++ src/components/paper-download.js
@@ -4,13 +4,13 @@ import PDFIcon from "../../static/img/learn/pdf-icon.svg";
 const PaperDownload = ({ title, header, image, link }) => {
   return (
     <>
-      <div className="col-span-1 rounded-2xl overflow-hidden">
-        <div className="relative w-full lg:h-28 md:h-20 h-28 flex flex-col items-center justify-center border-0 rounded-tr-2xl rounded-tl-2xl overflow-hidden bg-blue">
+      <div className="col-span-1 overflow-hidden">
+        <div className="relative w-full lg:h-28 md:h-20 h-28 flex flex-col items-center justify-center border-0 overflow-hidden">
           <div className="relative z-10 text-center">
-            <small className="lg:text-2xl md:text-base text-2xl text-white font-normal">
+            <small className="lg:text-2xl md:text-base text-2xl text-g font-normal">
               Tracer
             </small>
-            <h1 className="xl:text-3xl lg:text-2xl md:text-lg text-2xl text-white font-bold">
+            <h1 className="xl:text-2xl md:text-xl text-2xl text-white font-bold">
               {title}
             </h1>
           </div>
