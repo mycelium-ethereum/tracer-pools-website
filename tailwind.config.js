7f935da4c1c3b543d537de1fd323d25d96d032c9
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-11-26 13:55:43 +1000

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  // purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       colors: {
