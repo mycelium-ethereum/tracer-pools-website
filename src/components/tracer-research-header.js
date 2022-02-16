fd2fd7e384a267a825404f7e660f4ffaec7d41b8
Tim Jones <t27.jones@connect.qut.edu.au>
2022-02-16 17:10:15 +1000

    Fix Learn page heading font sizing

--- src/components/tracer-research-header.js
+++ src/components/tracer-research-header.js
@@ -6,13 +6,13 @@ const TracerResearchHeader = () => {
     <>
       <header className="relative z-20 mt-24 flex h-auto items-center md:items-end md:pt-0">
         <div className="container mx-auto flex w-full flex-col justify-center px-4 text-center lg:px-0">
-          <h1 className="mb-2 text-4xl font-black text-white transition-all duration-500 md:text-6xl">
+          <h1 className="mb-1 text-2xl font-black text-white transition-all duration-500 md:mb-3 md:text-[40px]">
             Tracer Research
           </h1>
-          <span className="text-lg font-normal text-white transition-all duration-500 md:text-2xl">
+          <small className="text-lg font-normal text-white transition-all duration-500 md:text-xl">
             Gain a more in-depth understanding of the Tracer
             <br /> Protocol and its capabilities.
-          </span>
+          </small>
         </div>
       </header>
     </>
