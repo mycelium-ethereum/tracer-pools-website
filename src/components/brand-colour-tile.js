1ad304329666f6891c71339b7f6306d44d84d1e1
Tim Jones <t27.jones@connect.qut.edu.au>
2022-02-17 13:10:03 +1000

    Add copy function to color hex codes

--- src/components/brand-colour-tile.js
+++ src/components/brand-colour-tile.js
@@ -1,17 +1,30 @@
 import React from "react";
+import CopyIcon from "../../static/img/brand/copy.svg";
 
 const BrandColourTile = ({ item, small }) => {
   const sizeStyles = small
     ? "min-h-[100px] xl:min-w-[266px]"
     : "min-h-[100px] xl:min-w-[366px]";
+  const copyToClipboard = (e, hex) => {
+    const textEl = e.target.querySelector("span");
+    navigator.clipboard.writeText(hex);
+    textEl.innerText = "Copied!";
+    setTimeout(() => {
+      textEl.innerText = hex;
+    }, 1000);
+  };
   return (
     <div
       style={{ backgroundColor: item.hex }}
       className={`mb-6 flex flex-col items-center justify-center rounded-[14px] ${sizeStyles} mx-3 min-w-full sm:min-w-[340px] xl:mx-0`}
     >
       <span className="text-sm text-white">{item.description}</span>
-      <button className="mt-2 flex items-center justify-center rounded-2xl bg-white px-2 py-1 text-[8px] text-black">
-        {item.hex}
+      <button
+        className="mt-2 flex items-center justify-center rounded-2xl bg-white px-2 py-1 text-[8px] text-black"
+        onClick={(e) => copyToClipboard(e, item.hex)}
+      >
+        <span className="pointer-events-none">{item.hex}</span>
+        <img src={CopyIcon} className="pointer-events-none ml-1" />
       </button>
     </div>
   );
