d9295e6e678b8491bc4247cd3515ec06d322c1b2
Tim Jones <t27.jones@connect.qut.edu.au>
2022-02-22 11:32:24 +1000

    Add new redirect, add forward slash to end of matomo URL in script.js

--- static/script.js
+++ static/script.js
@@ -7,8 +7,8 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="/mat";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    var u="/mat/";
+    _paq.push(['setTrackerUrl', '/mat-php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src='/mat-js'; s.parentNode.insertBefore(g,s);
