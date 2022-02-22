cbcfcded9615aceb72673c8580f1120c420bf6fa
Tim Jones <t27.jones@connect.qut.edu.au>
2022-02-22 11:25:20 +1000

    Add new redirects and script

--- static/script.js
+++ static/script.js
@@ -1,26 +1,22 @@
-let _paq = (window._paq = window._paq || []);
-_paq.push([
-  "setDomains",
-  [
-    "*.tracer.finance",
-    "*.docs.tracer.finance",
-    "*.pools.tracer.finance",
-    "*.vote.tracer.finance/",
-  ],
-]),
-  _paq.push(["enableCrossDomainLinking"]),
-  _paq.push(["trackPageView"]),
-  _paq.push(["enableLinkTracking"]),
-  (function () {
-    _paq.push(["setTrackerUrl", "/mat-php"]), _paq.push(["setSiteId", "2"]);
-    let e = document,
-      t = e.createElement("script"),
-      a = e.getElementsByTagName("script")[0];
-    (t.async = !0), (t.src = "/mat.js"), a.parentNode.insertBefore(t, a);
+// <!-- Matomo -->
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.tracer.finance"]);
+  _paq.push(["setDomains", ["*.tracer.finance","*.docs.tracer.finance","*.pools.tracer.finance","*.vote.tracer.finance/"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="/mat";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='/mat-js'; s.parentNode.insertBefore(g,s);
   })();
-let _mtm = (window._mtm = window._mtm || []);
-_mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-let d = document,
-  g = d.createElement("script"),
-  s = d.getElementsByTagName("script")[0];
-(g.async = !0), (g.src = "/mat-container.js"), s.parentNode.insertBefore(g, s);
+// <!-- End Matomo Code -->
+// <!-- Matomo Tag Manager -->
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='/mat-container-js'; s.parentNode.insertBefore(g,s);
+// <!-- End Matomo Tag Manager -->
