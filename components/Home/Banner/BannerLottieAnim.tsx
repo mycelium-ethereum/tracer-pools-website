2ca964d07ca37d29eed2db87611be48f6030450d
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-22 11:33:23 +1000

    Adjust sizing of lottie animation on large displays

--- components/Home/Banner/BannerLottieAnim.tsx
+++ components/Home/Banner/BannerLottieAnim.tsx
@@ -10,8 +10,8 @@ const BannerLottieAnim: React.FC<{}> = () => {
 
   const { View } = useLottie(options);
   return (
-    <div className="absolute top-0 right-0 z-10 h-full w-full translate-y-[150px] transform 4xl:translate-y-[200px] 4xl:scale-125">
-      <div className="bannerDiscAnim absolute top-0 right-0 h-full w-full">
+    <div className="absolute top-0 right-0 z-10 h-full w-full translate-y-[150px] -translate-x-[40vw] scale-[2.0] transform 4xl:translate-y-[900px] 4xl:-translate-x-[80vw] 4xl:scale-[3.5]">
+      <div className="bannerDiscAnim absolute top-0 right-0 h-full w-[1440px]">
         {View}
       </div>
     </div>
