c2536828374127cf7c498979582530a5504d9c7f
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-22 11:39:48 +1000

    Further adjustment of lottie animation

--- components/Home/Banner/BannerLottieAnim.tsx
+++ components/Home/Banner/BannerLottieAnim.tsx
@@ -10,8 +10,8 @@ const BannerLottieAnim: React.FC<{}> = () => {
 
   const { View } = useLottie(options);
   return (
-    <div className="absolute top-0 right-0 z-10 h-full w-full translate-y-[150px] -translate-x-[40vw] scale-[2.0] transform 4xl:translate-y-[900px] 4xl:-translate-x-[80vw] 4xl:scale-[3.5]">
-      <div className="bannerDiscAnim absolute top-0 right-0 h-full w-[1440px]">
+    <div className="absolute right-0 top-0 z-10 h-full w-full translate-y-[150px] scale-[2.0] transform 4xl:scale-[2.0]">
+      <div className="bannerDiscAnim absolute top-0 right-0 h-full w-full">
         {View}
       </div>
     </div>
