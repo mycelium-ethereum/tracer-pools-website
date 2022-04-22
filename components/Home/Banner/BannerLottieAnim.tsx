68135f8d7de78618e35e2097b51470d5e8b5ad0a
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-22 11:46:16 +1000

    Disable pointer events for Lottie animation

--- components/Home/Banner/BannerLottieAnim.tsx
+++ components/Home/Banner/BannerLottieAnim.tsx
@@ -10,7 +10,7 @@ const BannerLottieAnim: React.FC<{}> = () => {
 
   const { View } = useLottie(options);
   return (
-    <div className="absolute right-0 top-0 z-10 h-full w-full translate-y-[150px] scale-[2.0] transform 4xl:scale-[2.0]">
+    <div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-full translate-y-[150px] scale-[2.0] transform 4xl:scale-[2.0]">
       <div className="bannerDiscAnim absolute top-0 right-0 h-full w-full">
         {View}
       </div>
