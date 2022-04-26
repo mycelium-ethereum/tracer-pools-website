042efbf57071d90ccc2e71d65317bf4ba9c3d409
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-26 12:09:14 +1000

    Remove old renderer component

--- components/Radar/MarkdownContent.tsx
+++ components/Radar/MarkdownContent.tsx
@@ -5,19 +5,19 @@ import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 
 const MarkdownContent = (props) => {
-  const HeadingRenderer = ({ props }) => {
-    return (
-      <>
-        <h1 {...props}>{props.children}</h1>
-        <hr className="line-gradient-blue h-[1px] w-full border-none" />
-      </>
-    );
-  };
+  // const HeadingRenderer = ({ props }) => {
+  //   return (
+  //     <>
+  //       <h1 {...props}>{props.children}</h1>
+  //       <hr className="line-gradient-blue h-[1px] w-full border-none" />
+  //     </>
+  //   );
+  // };
 
   return (
     <ReactMarkdown
       {...props}
-      components={{ h3: HeadingRenderer }}
+      // components={{ h3: HeadingRenderer }}
       rehypePlugins={[rehypeRaw, remarkGfm, rehypeSlug]}
     />
   );
