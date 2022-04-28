5aa5a54a20f85b2da243470bcd61c9dfd7cf6d71
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-28 12:14:30 +1000

    Adjust positioning of lottie animation

--- components/Home/Banner/BannerLottieAnim.tsx
+++ components/Home/Banner/BannerLottieAnim.tsx
@@ -10,7 +10,7 @@ const BannerLottieAnim: React.FC<{}> = () => {
 
   const { View } = useLottie(options);
   return (
-    <div className="pointer-events-none absolute left-[800px] top-0 z-0 h-full w-full translate-y-[5vh] scale-[2.0] transform 4xl:left-[1000px] 4xl:translate-y-[55vh] 4xl:scale-[3.0] 5xl:left-[1500px] 5xl:translate-y-[75vh] 5xl:scale-[4.0]">
+    <div className="pointer-events-none absolute left-[800px] top-0 z-0 h-full w-full translate-y-[5vh] scale-[2.0] transform 3xl:left-[1100px] 3xl:translate-y-[10vh] 4xl:left-[3500px] 4xl:translate-y-[55vh] 4xl:scale-[4.0] 5xl:left-[3200px] 5xl:translate-y-[75vh] 5xl:scale-[4.0]">
       <div className="bannerDiscAnim absolute left-[1000px] top-1/4 h-[1920px] w-[1080px]">
         {View}
       </div>
