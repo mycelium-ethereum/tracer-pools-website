6073ec8d5a8880acb74b4bc45067ba0ba3cdb091
Tim Jones <t27.jones@connect.qut.edu.au>
2022-05-09 14:15:45 +1000

    Add styles for breakpoints for slider, second placeholder and autoplay

--- components/Pools/MechanismDesign/MechanismSlider.tsx
+++ components/Pools/MechanismDesign/MechanismSlider.tsx
@@ -13,7 +13,7 @@ const MechanismSlider: React.FC<{}> = () => {
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
-      image: "/img/pools/mechanism-design-placeholder.png",
+      image: "/img/pools/mechanism-design-placeholder-2.png",
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
@@ -21,7 +21,7 @@ const MechanismSlider: React.FC<{}> = () => {
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
-      image: "/img/pools/mechanism-design-placeholder.png",
+      image: "/img/pools/mechanism-design-placeholder-2.png",
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
@@ -29,7 +29,7 @@ const MechanismSlider: React.FC<{}> = () => {
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
-      image: "/img/pools/mechanism-design-placeholder.png",
+      image: "/img/pools/mechanism-design-placeholder-2.png",
       text: "When there is more collateral in one side of the pool, traders taking the less popular position benefit from a greater effective leverage on gains than they experience on losses.",
     },
     {
@@ -44,9 +44,9 @@ const MechanismSlider: React.FC<{}> = () => {
     dots: false,
     arrows: false,
     vertical: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 500,
     draggable: false,
     swipe: false,
     touchMove: false,
@@ -61,53 +61,53 @@ const MechanismSlider: React.FC<{}> = () => {
     arrows: false,
     // infinite: true,
     vertical: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 500,
     draggable: false,
     swipe: false,
     touchMove: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // fade: true,
+    fade: true,
   };
 
   const Slide: React.FC<{ item: { image: string; text: string } }> = ({
     item,
   }) => (
-    <div className="p-12">
+    <div className="py-10 pr-10 3xl:px-12 3xl:pt-12">
       {/* Will be replaced by Lottie animations in future */}
       <img
         src={item.image}
         alt=""
-        className="h-[585px] w-full object-cover object-center"
+        className="h-[324px] w-full object-cover object-center 3xl:h-[585px]"
       />
-      <p className="mt-4 text-center text-lg leading-[28px] text-action-active">
+      <p className="mt-4 text-action-active 3xl:text-center 3xl:text-xl 3xl:leading-[28px]">
         {item.text}
       </p>
     </div>
   );
 
   const SlideNavLink: React.FC<{ num: number }> = ({ num }) => (
-    <button className="relative my-[22px] text-[40px] font-light leading-[44px] text-action-inactive transition-colors duration-300">
+    <button className="relative my-[15px] text-2xl font-light leading-[30px] text-action-inactive transition-colors duration-300 3xl:my-[22px] 3xl:text-[40px] 3xl:leading-[44px]">
       {num}
     </button>
   );
 
   return (
-    <div className="relative ml-10 flex w-full max-w-[1477px] items-center border border-action-active [box-shadow:_0px_4px_10px_2px_rgba(26,85,245,0.1)]">
-      <div className="relative ml-4 mr-16 min-w-[42px]">
+    <div className="relative z-10 ml-10 flex w-full max-w-[767px] items-center border border-action-active bg-white [box-shadow:_0px_4px_10px_2px_rgba(26,85,245,0.1)] 3xl:max-w-[1477px]">
+      <div className="relative ml-[23px] mr-10 min-w-[43px] 3xl:ml-4 3xl:mr-16 3xl:min-w-[42px]">
         <Slider
           asNavFor={slider2}
           ref={(slider) => setSlider1(slider)}
           {...sliderNavSettings}
-          className="slider-nav relative z-10 h-[635px] w-[42px]"
+          className="slider-nav relative z-10 h-[440px] w-[43px] 3xl:h-[635px] 3xl:w-[42px]"
         >
           {Array.from({ length: TOTAL_SLIDES }).map((slideNum: number, i) => (
             <SlideNavLink key={i} num={i + 1} />
           ))}
         </Slider>
-        <span className="slider-nav-gradient absolute right-0 top-1/2 block h-[calc(100%+60px)] w-[1px] flex-grow -translate-y-1/2" />
+        <span className="slider-nav-gradient absolute right-0 top-1/2 block h-full w-[1px] flex-grow -translate-y-1/2 3xl:h-[calc(100%+60px)]" />
       </div>
       <Slider
         asNavFor={slider1}
