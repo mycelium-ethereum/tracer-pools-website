f2469f5a56d2b4b0d34c4359ab04766f1aa915d0
Tim Jones <t27.jones@connect.qut.edu.au>
2022-05-12 16:42:36 +1000

    Adjust Call to Action content on extra large displays

--- components/Radar/Post/CallToAction.tsx
+++ components/Radar/Post/CallToAction.tsx
@@ -3,7 +3,7 @@ import Button from "../../Shared/Buttons/Button";
 
 const CallToAction: React.FC<{}> = ({}) => {
   return (
-    <div className="relative flex h-[204px] w-full items-center justify-center overflow-hidden rounded-lg bg-tracer-navy py-6 px-6 lg:mb-4  lg:h-[84px] lg:overflow-visible lg:py-7 xl:justify-start 2xl:pl-14">
+    <div className="relative flex h-[204px] w-full items-center justify-center overflow-hidden rounded-lg bg-tracer-navy py-6 px-6 lg:mb-4 lg:h-[84px] lg:overflow-visible lg:py-7 xl:justify-start 2xl:pl-14 3xl:justify-center">
       <img
         src="/img/radar/cta-bg.png"
         className="absolute top-0 left-0 z-0 hidden h-full w-full object-cover object-right lg:block"
@@ -12,7 +12,7 @@ const CallToAction: React.FC<{}> = ({}) => {
         src="/img/radar/cta-bg-mobile.png"
         className="absolute top-0 right-0 z-0 block h-full w-[359px] lg:hidden"
       />
-      <div className="relative z-10 flex flex-col items-center xl:flex-row">
+      <div className="relative z-10 flex flex-col items-center xl:flex-row 3xl:mr-20">
         <img src="/img/logos/tracer-perpetual-pools.svg" className="mr-4 h-6" />
         <h2 className="mt-2 text-xl leading-[28px] text-white lg:mt-0">
           More Leverage. Less Overhead.
