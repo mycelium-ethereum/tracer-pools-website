5b6f7654e3b5ed041907c35d1640065860b8f313
Tim Jones <t27.jones@connect.qut.edu.au>
2022-06-06 15:35:07 +1000

    Prevent Mechanism Design Step Lottie animation from looping

--- components/Pools/MechanismDesign/Slide.tsx
+++ components/Pools/MechanismDesign/Slide.tsx
@@ -4,18 +4,20 @@ import StepLottieAnim from "@components/Pools/MechanismDesign/StepLottieAnim";
 const Slide: React.FC<{
   item: { image: string; lottieFile?: any; text: string };
   currentSlide: number;
-}> = ({ item, currentSlide }) => (
-  <div className="flex h-full flex-col-reverse justify-center p-6 sm:flex-col sm:py-10 sm:pr-10 4xl:px-12 4xl:pt-12">
-    {item.image && (
-      <OptimisedImage src={item.image} alt="" className="h-full" />
-    )}
-    {item.lottieFile && (
-      <StepLottieAnim lottieFile={item.lottieFile} key={currentSlide} />
-    )}
-    <p className="mx-auto mb-4 h-[100px] max-w-[818px] text-action-active sm:mt-4 sm:mb-0 sm:h-[50px] xl:h-[100px] 4xl:h-[50px] 4xl:text-center 4xl:text-xl 4xl:leading-[28px]">
-      {item.text}
-    </p>
-  </div>
-);
+}> = ({ item, currentSlide }) => {
+  return (
+    <div className="flex h-full flex-col-reverse justify-center p-6 sm:flex-col sm:py-10 sm:pr-10 4xl:px-12 4xl:pt-12">
+      {item.image && (
+        <OptimisedImage src={item.image} alt="" className="h-full" />
+      )}
+      {item.lottieFile && (
+        <StepLottieAnim lottieFile={item.lottieFile} key={currentSlide} />
+      )}
+      <p className="mx-auto mb-4 h-[100px] max-w-[818px] text-action-active sm:mt-4 sm:mb-0 sm:h-[50px] xl:h-[100px] 4xl:h-[50px] 4xl:text-center 4xl:text-xl 4xl:leading-[28px]">
+        {item.text}
+      </p>
+    </div>
+  );
+};
 
 export default Slide;
--- components/Pools/MechanismDesign/StepLottieAnim.tsx
+++ components/Pools/MechanismDesign/StepLottieAnim.tsx
@@ -1,15 +1,36 @@
-import { useLottie } from "lottie-react";
+import { useEffect, useRef } from "react";
+import Lottie from "lottie-react";
+import { useOnScreen } from "hooks";
 
 const StepLottieAnim: React.FC<{ lottieFile: string }> = ({ lottieFile }) => {
-  const options = {
-    animationData: lottieFile,
-    // loop: false,
-    autoplay: true,
-  };
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lottieRef = useRef(null);
 
-  const { View } = useLottie(options);
+  const onScreen = useOnScreen(containerRef);
 
-  return <div className="h-auto w-full">{View}</div>;
+  useEffect(() => {
+    if (onScreen) {
+      lottieRef.current.play();
+      setTimeout(() => {
+        lottieRef.current.pause();
+      }, 2500);
+    }
+  }, [onScreen]);
+
+  useEffect(() => {
+    lottieRef.current.goToAndStop(0, true);
+  }, []);
+
+  return (
+    <div className="h-auto w-full" ref={containerRef}>
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={lottieFile}
+        autoPlay={false}
+        loop={false}
+      />
+    </div>
+  );
 };
 
 export default StepLottieAnim;
