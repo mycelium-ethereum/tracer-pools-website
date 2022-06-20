b309a4ec72c51248d05e457c945ca6296da44248
Tim Jones <t27.jones@connect.qut.edu.au>
2022-06-20 23:04:29 +1000

    Adjust GetPaid section header margin

--- components/Indices/GetPaid/Header.tsx
+++ components/Indices/GetPaid/Header.tsx
@@ -1,25 +1,25 @@
-import UnderlinedHeading from "@components/Shared/Heading/UnderlinedHeading";
+import UnderlinedHeading from "@components/Shared/Heading/UnderlinedHeading"
 
 const Header: React.FC<{}> = () => {
-  return (
-    <header className="text-action-active sm:max-w-[455px] 4xl:max-w-[581px]">
-      <UnderlinedHeading lineColor="blue" className="max-w-[294px]">
-        GET PAID FOR YOUR CREATIVITY
-      </UnderlinedHeading>
-      <h2 className="mb-4 max-w-[455px] text-2xl leading-[28px] 3xl:max-w-full 4xl:text-[40px] 4xl:leading-[44px]">
-        <span className="inline-block text-[40px] sm:leading-[44px] 4xl:text-[64px] 4xl:leading-[72px]">
-          10% of fees
-        </span>
-        <br />
-        earned on a Perpetual Pool market get sent to the deployer of the price
-        feed.
-      </h2>
-      <p className="max-w-[305px] text-tertiary">
-        The more highly demanded the data you bring on chain, the greater the
-        reward.
-      </p>
-    </header>
-  );
-};
+    return (
+        <header className="mr-[180px] text-action-active sm:max-w-[455px] 4xl:max-w-[581px]">
+            <UnderlinedHeading lineColor="blue" className="max-w-[294px]">
+                GET PAID FOR YOUR CREATIVITY
+            </UnderlinedHeading>
+            <h2 className="mb-4 max-w-[455px] text-2xl leading-[28px] 3xl:max-w-full 4xl:text-[40px] 4xl:leading-[44px]">
+                <span className="inline-block text-[40px] sm:leading-[44px] 4xl:text-[64px] 4xl:leading-[72px]">
+                    10% of fees
+                </span>
+                <br />
+                earned on a Perpetual Pool market get sent to the deployer of
+                the price feed.
+            </h2>
+            <p className="max-w-[305px] text-tertiary">
+                The more highly demanded the data you bring on chain, the
+                greater the reward.
+            </p>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
--- components/Indices/GetPaid/index.tsx
+++ components/Indices/GetPaid/index.tsx
@@ -1,20 +1,20 @@
-import Section from "@components/Shared/Section";
-import Container from "@components/Shared/Container";
-import Header from "@components/Indices/GetPaid/Header";
-import TenPercentLottieAnim from "@components/Indices/GetPaid/TenPercentLottieAnim";
+import Section from "@components/Shared/Section"
+import Container from "@components/Shared/Container"
+import Header from "@components/Indices/GetPaid/Header"
+import TenPercentLottieAnim from "@components/Indices/GetPaid/TenPercentLottieAnim"
 
 const GetPaid: React.FC<{}> = () => {
-  return (
-    <Section
-      id="section-2"
-      className="h-[650px] bg-white pt-16 lg:pt-0 4xl:h-[1000px]"
-    >
-      <Container className="flex h-full justify-between lg:items-center">
-        <Header />
-        <TenPercentLottieAnim />
-      </Container>
-    </Section>
-  );
-};
+    return (
+        <Section
+            id="section-2"
+            className="h-[650px] bg-white pt-16 lg:pt-0 4xl:h-[1000px]"
+        >
+            <Container className="flex h-full lg:items-center">
+                <Header />
+                <TenPercentLottieAnim />
+            </Container>
+        </Section>
+    )
+}
 
-export default GetPaid;
+export default GetPaid
