ed80e4cfb7ba9fbd1d4c3c08a523238f6e9b8541
Tim Jones <t27.jones@connect.qut.edu.au>
2022-08-09 13:54:14 +1000

    Update classNames with classNames library

--- src/components/mycelium-modal/index.js
+++ src/components/mycelium-modal/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { WaitlistForm, LoadingSplash, SuccessSplash } from "./states";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import cx from "classnames";
 
 const POPUP_DELAY = 2000;
 const postUrl = `https://xyz.us11.list-manage.com/subscribe/post?u=71f7321770f187b58dc7d4552&amp;id=53e1b02275&amp;f_id=005b8ee0f0`;
@@ -52,11 +53,19 @@ const MyceliumPopup = () => {
       url={postUrl}
       render={({ subscribe, status, message }) => (
         <div
-          className={`mycelium-modal fixed top-0 left-0 z-[51] flex h-screen w-full items-center justify-center bg-[#001100]/80 px-4 transition-opacity duration-500 md:px-0 ${
-            isOpen ? activeStyles : inactiveStyles
-          }`}
+          className={cx(
+            "mycelium-modal fixed top-0 left-0 z-[51] flex h-screen w-full items-center justify-center bg-[#001100]/80 px-4 transition-opacity duration-500 md:px-0",
+            {
+              activeStyles: isOpen,
+              inactiveStyles: !isOpen,
+            }
+          )}
         >
-          <div className="relative max-h-[calc(100%-40px)] w-full max-w-[644px] overflow-y-auto overflow-x-hidden rounded-lg bg-[#001700] px-8 pt-12 pb-10 font-aileron text-white md:overflow-y-hidden">
+          <div
+            className={cx(
+              "relative max-h-[calc(100%-40px)] w-full max-w-[644px] overflow-x-hidden rounded-lg bg-[#001700] px-8 pt-12 pb-10 font-aileron text-white md:overflow-y-hidden"
+            )}
+          >
             <WaitlistForm
               userInput={userInput}
               setUserInput={setUserInput}
